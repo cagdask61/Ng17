197cd215edc635339776be7ef67d19522f91d790
Cagdas <kocamancagdas59@gmail.com>
2023-11-25 20:59:46 +0300

    added examples

---
+++ src/app/components/examples/examples.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-examples',
+  standalone: true,
+  templateUrl: './examples.component.html',
+  styleUrl: './examples.component.scss',
+  imports: [],
+})
+export default class ExamplesComponent {
+
+}
